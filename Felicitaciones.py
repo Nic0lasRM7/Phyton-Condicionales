e94238867947084ec82b953d28cc1984ebfa3c8b
Nic0lasRM7 <nicolasruizmedina08@gmail.com>
2025-11-10 16:27:01 -0500

    Add files via upload

---
+++ Felicitaciones.py
@@ -0,0 +1,6 @@
+definitiva= float(input("Ingrese la nota definitiva: "))
+print("Su definitiva es: ", definitiva)
+if definitiva >= 4:
+    print("Felicitaciones")
+elif definitiva < 4 and definitiva >= 0:
+    print("q malo")
